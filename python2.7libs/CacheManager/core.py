98b8cc2ee2fb3b34e875f2eab29a63c4cec3efad
Takanori Kishikawa <auratus.lemma@gmail.com>
2016-08-18 22:13:53 +0900

    Add node type recognition.
    For "file", "filecache", "alembic", "alembicarchive".

--- python2.7libs/CacheManager/core.py
+++ python2.7libs/CacheManager/core.py
@@ -14,7 +14,7 @@ from . import define as Define
 reload(Define)
 
 #-------------------------------------------------------------------------------
-# Houdini data management class
+# Core API to access to Houdini Datas
 #-------------------------------------------------------------------------------
 class houManager(object):
     """Core API to access to Houdini Datas
@@ -29,18 +29,22 @@ class houManager(object):
         all_nodes = hou.pwd().allSubChildren()
 
         for node in all_nodes:
+
             if node.type().name().lower() in Define.CACHE_NODES:
 
                 eachNode_dict     = {}
                 node_path         = node.path()
-                cachePath         = self.unexpStrPath(node_path)
+                node_type         = node.type().name().lower()
+                cachePath         = self.unexpStrPath(node_path, node_type)
+                evalCachePath     = self.evalStrPath(node, node_type)
 
                 eachNode_dict["name"]           = node.name()
                 eachNode_dict["node_path"]      = node_path
-                eachNode_dict["cache_path"]     = self.unexpStrPath(node_path)
+                eachNode_dict["cache_path"]     = cachePath
                 eachNode_dict["env"]            = self.analizeEnv(cachePath)
-                eachNode_dict["expanded_path"]  = node.evalParm("file")
-                eachNode_dict["color"]         = node.color().rgb()
+                eachNode_dict["expanded_path"]  = evalCachePath
+                eachNode_dict["color"]          = node.color().rgb()
+                eachNode_dict["editable"]       = True
 
                 current_cache_nodes.append(eachNode_dict)
 
@@ -48,14 +52,38 @@ class houManager(object):
 
 
     @classmethod
-    def unexpStrPath(self, path=""):
-        cachePath = path + "/file"
-        unExpPath = hou.parm(cachePath).unexpandedString()
-        return unExpPath
+    def unexpStrPath(self, path, opType):
+        if opType == "file" or "filecache":
+            parmPath = path + "/file"
+            unExpPath = hou.parm(parmPath).unexpandedString()
+
+        elif opType == "alembic" or "alembicarchive":
+            parmPath = path + "/fileName"
+            unExpPath = hou.parm(parmPath).unexpandedString()
+
+        try:
+            return unExpPath
+        except:
+            return None
 
 
     @classmethod
-    def analizeEnv(self, path=""):
+    def evalStrPath(self, node, opType):
+
+        if opType == "file" or "filecache":
+            evalPath = node.evalParm("file")
+
+        elif opType == "alembic" or "alembicarchive":
+            evalPath = node.evalParm("fileName")
+
+        try:
+            return evalPath
+        except:
+            return None
+
+
+    @classmethod
+    def analizeEnv(self, path):
         pathParts = path[0].split('/')
         if pathParts[0] == None:
             return "-"
@@ -63,11 +91,35 @@ class houManager(object):
             return pathParts[0]
 
 
+    @classmethod
+    def isEditableNode(self, path):
+        try:
+            node = hou.node(path)
+
+        except IndexError:
+            return None
+
+        if node.type().name.lower() in Define.CHILDNODES_EXCEPTION:
+            pathTokens = path.split('/')
+
+            for idx in len(pathTokens):
+                pathTokens.pop(-idx)
+                node_path = '/' + '/'.join(pathTokens)
+
+                node_type = hou.node(node_path).type().name().lower()
+
+                if node_type in Define.CHILDNODES_EXCEPTION:
+                    pass
+
+
+
+
 #-------------------------------------------------------------------------------
 # OS file management class
 #-------------------------------------------------------------------------------
 class fileManager(object):
-    """docstring for fileManager"""
+    """OS file management class
+    """
     def __init__(self):
         super(fileManager, self).__init__()
 

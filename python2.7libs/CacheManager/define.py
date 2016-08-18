0161e07184b48d4c419adfac32eb8bb2225ff0b2
Takanori Kishikawa <auratus.lemma@gmail.com>
2016-08-18 22:11:30 +0900

    Add node exceptions.

--- python2.7libs/CacheManager/define.py
+++ python2.7libs/CacheManager/define.py
@@ -6,37 +6,34 @@ Define file for Cache Manager Tool.
 """
 #-------------------------------------------------------------------------------
 
-# Define Cache Nodes to deal with this script
+## Define Cache Nodes to deal with this script.
 CACHE_NODES = [
     "file",
     "filecache",
-    "alembic",
-    "alembicarchive"
+    # "alembic",
+    # "alembicarchive",
 ]
 
 
-# Define Houdini Environment Varialbes. This will also be used for displaying.
+## Define Houdini Environment Varialbes. This will also be used for displaying.
 ENV_TYPE = [
     '-',
     'HIP',
-    'JOB'
-]
-
-# Define Header Items
-CACHE_ITEMS = [
-    { "key": "name",           "display": "Name",           "width": 100,  "visible": False},
-    { "key": "node",           "display": "Node",           "width": 200,  "visible": True},
-    { "key": "cache_path",     "display": "Cache Path",     "width": 500,  "visible": True},
-    { "key": "srcStatus",      "display": "Status",         "width": 50,  "visible": True},
-    { "key": "env",            "display": "Env",            "width": 50,   "visible": False},
-    { "key": "expanded_path",  "display": "Expanded path",  "width": 200,  "visible": False},
-    { "key": "color",          "display": "Color",          "width": None, "visible": False}
+    'JOB',
 ]
 
 
-# Menu Items
+## Menu Items.
 MENU_HELP = "Help"
 MENU_RELOAD = "Reload"
+
+## Listed CHACHE_NODES node has children which should be got rid of as default.
+PARENTNODES_EXCEPTION = [
+    "filecache",
+    "hlight",
+    "arnold_light",
+
+]
 
 #-------------------------------------------------------------------------------
 # EOF

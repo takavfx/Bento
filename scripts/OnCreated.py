13f0ff2bacb632502ea6d3fa0b298233eae4bda3
Takanori Kishikawa <auratus.lemma@gmail.com>
2016-08-23 21:41:14 +0900

    Add OnCreated script file to test behaviour when def-nodes are created.

---
+++ scripts/OnCreated.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+## Description
+"""
+This python script works when each node is created.
+"""
+#-------------------------------------------------------------------------------
+
+# node = kwargs["node"]
+# node_type = node.type()
+# node_type_name = node_type.name()
+# type_category_name = node_type.category().name().lower()
+
+# pypanels = hou.pypanel.interfaces()
+#
+# if pypanels.has_key("cacheManager"):
+#     cm = pypanels.get("cacheManager")
+#     print cm, node_type_name
+    # cm.showToolbar(show)

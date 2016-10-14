46961c600c52e070270c41f784ebd51f4fa4aef9
Takanori Kishikawa <auratus.lemma@gmail.com>
2016-10-15 01:20:35 +0900

    test update

--- scripts/OnCreated.py
+++ scripts/OnCreated.py
@@ -5,3 +5,18 @@
 This script runs when Houdini nodes are created.
 """
 #-------------------------------------------------------------------------------
+
+import hou
+
+input_node_type = {
+
+}
+
+
+output_node_type = {
+
+}
+
+input_node_type_color  = (1, 0.8, 0)
+
+output_node_type_color = ()
